bbc2e3dbf09b375fe2beb09afd610fed10e362fe
Asus <mehmetjavaderstakip@gmail.com>
2023-10-07 16:29:13 +0300

    first lesson passed

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        int milis = 1;
 
         driver.get("https://google.com");
         WebElement searchGoogle = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
@@ -14,13 +15,30 @@ public class Main {
         searchGoogle.sendKeys("https://demoqa.com/");
         searchGoogle.sendKeys(Keys.ENTER);
         WebElement demoqaSelector = driver.findElement(By.xpath("//h3[text()='DEMOQA']"));
-        demoqaSelector.click();Thread.sleep(2000);
+        demoqaSelector.click();Thread.sleep(milis);
         JavascriptExecutor jsx = (JavascriptExecutor) driver;
         jsx.executeScript("window.scrollBy(0, 300)");
         WebElement element = driver.findElement(By.xpath("//h5[text()='Elements']"));
         element.click();
         WebElement webTables = driver.findElement(By.xpath("//span[text()='Web Tables']"));
-        webTables.click();
+        webTables.click();Thread.sleep(milis);
+        WebElement addButton = driver.findElement(By.id("addNewRecordButton"));
+        addButton.click();Thread.sleep(milis);
+        WebElement firstName = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+        firstName.sendKeys("hasan");
+        WebElement lastName = driver.findElement(By.cssSelector("#lastName"));
+        lastName.sendKeys("hacı");
+        WebElement email = driver.findElement(By.xpath("//input[@id='userEmail']"));
+        email.sendKeys("aha@gmail.com");
+        WebElement age = driver.findElement(By.xpath("//input[@id='age']"));
+        age.sendKeys("40");
+        WebElement salary = driver.findElement(By.xpath("//input[@id='salary']"));
+        salary.sendKeys("1000");
+        WebElement department = driver.findElement(By.xpath("//input[@id='department']"));
+        department.sendKeys("havaCiva");
+        WebElement submit = driver.findElement(By.xpath("//button[@id='submit']"));
+        submit.click();
+        jsx.executeScript("window.scrollBy(0, 300)");Thread.sleep(milis);
 
 
 
