c09d7673ecf3f5f05260dd3fc400c35992746e75
Asus <mehmetjavaderstakip@gmail.com>
2023-10-10 13:40:56 +0300

    raport runned

---
+++ src/test/java/TestNG.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test(groups = {"Group A"}, priority = 1, enabled = true)
+    public void basariliMailTesti(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+    @Test(groups = {"Group B"}, priority = 2)
+    public void test1(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+    @Test(groups = {"Group A", "Group B"})
+    public void test2(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+    @Test(groups = {"Group C"}, priority = 3, enabled = true)
+    public void test3(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+}
